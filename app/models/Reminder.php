eeba5c5f958fe63272a6a2676988d7a7b08222c9
QiuruiWu4806 <qiurwu@algomau.ca>
2024-06-28 14:01:57 +0000

    Finished method create_reminder, it will take in the value for subject and get the id of the current login user to create a new reminder

--- app/models/Reminder.php
+++ app/models/Reminder.php
@@ -15,6 +15,24 @@ class Reminder {
     }
 
     public function update_reminder ($reminder_id) {
+      
+    }
 
+    public function delete_reminder ($reminder_id) {
+
+    }
+
+    public function create_reminder ($subject){
+      $db = db_connect();
+      $statement = $db->prepare("INSERT INTO reminders (user_id, subject) VALUES (:user_id, :subject);");
+      
+      $user_id = $db->prepare("select * from users WHERE username = :name;");
+      $user_id -> bindValue(':name', $_SESSION['username']);
+      $user_id -> execute();
+      $rows = $user_id -> fetch(PDO::FETCH_ASSOC);
+      
+      $statement->bindValue(':user_id', $rows[id]);
+      $statement->bindValue(':subject', $subject);
+      $statement->execute();
     }
 }
